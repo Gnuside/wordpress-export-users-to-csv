b55c1e09f36be13b9dcdb0f946bf8a2b97fe451c
Adrien Vossough <adrien.voss@gmail.com>
2014-08-11 10:39:46 +0200

    Adding meta.

--- export-users-to-csv.inc.php
+++ export-users-to-csv.inc.php
@@ -4,6 +4,10 @@ $gnuside_eutcvs_meta_cfg = array(
 	'first_name'  => '',
 	'last_name'   => '',
 	'nickname'    => '',
+	'description' => '',
+	'company'     => '',
+	'function'    => '',
+	'phone_nbr'   => ''
 );
 
 return $gnuside_eutcvs_meta_cfg;
